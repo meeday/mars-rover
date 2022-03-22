f72f69bf3efc23cc9b11eb3e851719cb7507ceef
Meedaxa <meedaxa@synaptek.ninja>
2022-03-22 19:17:50 +0000

    function to display grid console

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ const createNewGrid = (row, column) => {
     const grid = new Array(row).fill("-").map(() => new Array(column).fill("-"));
     return grid;
 }
+
+/**
+ * @param {array} grid
+ */
+const displayGrid = (grid) => {
+    for (let r = 0; r < grid.length; r++) {
+        for (let c = 0; c < grid[0].length; c++) {
+            process.stdout.write(grid[r][c]);
+            process.stdout.write(" ");
+        }
+        process.stdout.write("\n");
+    }
+}
+
