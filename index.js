0dedc031c35849a6088902b81f27ff91a6c326bf
Meedaxa <meedaxa@synaptek.ninja>
2022-03-23 11:08:06 +0000

    move rover functions

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /**
+ * @description function to create 2D array(grid)
  * @param {int} row
  * @param {int} column
  * @return {*} 2D array
@@ -9,6 +10,7 @@ const createNewGrid = (row, column) => {
 }
 
 /**
+ * @description function to display grid in console
  * @param {array} grid
  */
 const displayGrid = (grid) => {
@@ -23,11 +25,11 @@ const displayGrid = (grid) => {
 }
 
 /**
- * @description functions to move rover north
+ * @description function to move rover north
  * @param {array} grid
  * @param {int} currentRow
  * @param {int} currentColumn
- * @return {array} new Position
+ * @return {array} new position
  */
 const moveNorth = (grid, currentRow, currentColumn) => {
     const newRow = currentRow + 1
@@ -38,5 +40,59 @@ const moveNorth = (grid, currentRow, currentColumn) => {
     // to make tracking rover easier. "x" symbolises the rovers previous moves and "O" symbolises the rovers current postion
     grid[currentRow][currentColumn] = "x"
     grid[newRow][currentColumn] = "O"
+    return newPosition
+}
+
+/**
+ * @description function to move rover south
+ * @param {array} grid
+ * @param {int} currentRow
+ * @param {int} currentColumn
+ * @return {array} new position
+ */
+const moveSouth = (grid, currentRow, currentColumn) => {
+    const newRow = currentRow - 1
+    const newPosition = [newRow, currentColumn]
+    if (newRow < 0 ) {
+        return "Lost"
+    }
+    grid[currentRow][currentColumn] = "x"
+    grid[newRow][currentColumn] = "O"
+    return newPosition
+}
+
+/**
+ * @description function to move rover west
+ * @param {array} grid
+ * @param {int} currentRow
+ * @param {int} currentColumn
+ * @return {array} new position
+ */
+const moveWest = (grid, currentRow, currentColumn) => {
+    const newColumn = currentColumn - 1
+    const newPosition = [currentRow, newColumn]
+    if (newColumn < 0) {
+        return "Lost"
+    }
+    grid[currentRow][currentColumn] = "x"
+    grid[currentRow][newColumn] = "O"
+    return newPosition
+}
+
+/**
+ * @description function to move rover east
+ * @param {array} grid
+ * @param {int} currentRow
+ * @param {int} currentColumn
+ * @return {array} new position
+ */
+const moveEast = (grid, currentRow, currentColumn) => {
+    const newColumn = currentColumn + 1
+    const newPosition = [currentRow, newColumn]
+    if (newColumn > grid[0].length - 1) {
+        return "Lost" 
+    }
+    grid[currentRow][currentColumn] = "x"
+    grid[currentRow][newColumn] = "O"
     return newPosition
 }
