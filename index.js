393343a867aa31d603c80d13464535aa89ce528f
Meedaxa <meedaxa@synaptek.ninja>
2022-03-23 11:00:48 +0000

    display grid edited to have starting point at 0,0 and Move north function created

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const createNewGrid = (row, column) => {
  * @param {array} grid
  */
 const displayGrid = (grid) => {
-    for (let r = 0; r < grid.length; r++) {
+    // decrement row so that 0,0 is in the bottom left corner
+    for (let r = grid.length - 1; r >= 0; r--) {
         for (let c = 0; c < grid[0].length; c++) {
             process.stdout.write(grid[r][c]);
             process.stdout.write(" ");
@@ -21,3 +22,21 @@ const displayGrid = (grid) => {
     }
 }
 
+/**
+ * @description functions to move rover north
+ * @param {array} grid
+ * @param {int} currentRow
+ * @param {int} currentColumn
+ * @return {array} new Position
+ */
+const moveNorth = (grid, currentRow, currentColumn) => {
+    const newRow = currentRow + 1
+    const newPosition = [newRow, currentColumn]
+    if(newRow > grid.length -1){
+        return "Lost"
+    }
+    // to make tracking rover easier. "x" symbolises the rovers previous moves and "O" symbolises the rovers current postion
+    grid[currentRow][currentColumn] = "x"
+    grid[newRow][currentColumn] = "O"
+    return newPosition
+}
